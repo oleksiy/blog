65e059eafa48f9247e901952c3e34ef1af5c94e9
Oleksiy Blavat <oblavat@invoca.com>
2023-04-28 23:38:32 -0400

    new routes added

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/") to the ArticlesController#index action
   root "articles#index"
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+  #get "/articles", to: "articles#index"
+  #get "/articles/:id", to: "articles#show"
+  resources :articles
 end
