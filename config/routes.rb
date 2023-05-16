ac29df288dfeca722d0146c918a42cef68681ed4
Oleksiy Blavat <oblavat@invoca.com>
2023-05-16 00:51:28 -0400

    Adding comments to articles

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   root "articles#index"
   #get "/articles", to: "articles#index"
   #get "/articles/:id", to: "articles#show"
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
+
 end
