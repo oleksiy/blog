bc961a84a605d42ea4a0f52314b9e05a899b046e
Oleksiy Blavat <blavat.alex@gmail.com>
2024-11-13 01:31:20 -0500

    added concerns/privacy for articles

---
+++ app/models/concerns/visible.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Visible
+  extend ActiveSupport::Concern
+
+  VALID_STATUSES = %w[public, private, archived]
+
+  included do
+    validates :status, inclusion: { in: VALID_STATUSES }
+  end
+  def archived?
+    status == "archived"
+  end
+end
