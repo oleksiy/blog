775b82a8b68e0af41f6f021cf2e3265d57930dec
Oleksiy Blavat <blavat.alex@gmail.com>
2024-11-13 19:13:59 -0500

    Chapter 10 (#5) :eggplant:
    * initial commit :iphone:
    
    * ignoring mac folder ids, fixed the partial form for creating new articles. Added comment deletion & article deletion :newspaper:

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,3 @@
-=begin
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # Defines the root path route ("/") to the ArticlesController#index action
-  root "articles#index"
-  #get "/articles", to: "articles#index"
-  #get "/articles/:id", to: "articles#show"
-  resources :articles do
-    resources :comments
-  end
-
-end
-=end
 Rails.application.routes.draw do
   root "articles#index"
   resources :articles do
